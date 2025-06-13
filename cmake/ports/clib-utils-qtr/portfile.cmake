ca2a99467d201c7fe13282f95747e524e85e6a79
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-06-13 04:12:56 +0200

    name changes

--- cmake/ports/clib-utils-qtr/portfile.cmake
+++ cmake/ports/clib-utils-qtr/portfile.cmake
@@ -1,13 +1,13 @@
 # header-only library
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO QY-MODS/ClibUtilsQY
+    REPO QTR-Modding/CLibUtilsQTR
     REF 0
     SHA512 0
     HEAD_REF main
 )
 
 # Install codes
-set(CLIBUTILSQY_SOURCE	${SOURCE_PATH}/include/ClibUtilsQY)
-file(INSTALL ${CLIBUTILSQY_SOURCE} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+set(CLibUtilsQTR_SOURCE	${SOURCE_PATH}/include/CLibUtilsQTR)
+file(INSTALL ${CLibUtilsQTR_SOURCE} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
