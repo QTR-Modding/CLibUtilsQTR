a8d9534ef84bdd1743c687feb7e6f97adfc9014f
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-07-04 09:44:08 +0200

    Add Papyrus.hpp
    Introduced a new Papyrus.hpp header providing utility functions for Papyrus VM interactions. Updated cmake/headerlist.cmake and utils.hpp to include the new header.

--- cmake/headerlist.cmake
+++ cmake/headerlist.cmake
@@ -3,4 +3,5 @@ set(headers ${headers}
 	include/CLibUtilsQTR/Tasker.hpp
 	include/CLibUtilsQTR/PresetSettings.hpp
 	include/CLibUtilsQTR/FormReader.hpp
+	include/CLibUtilsQTR/Papyrus.hpp
 )
--- include/ClibUtilsQTR/utils.hpp
+++ include/ClibUtilsQTR/utils.hpp
@@ -4,3 +4,4 @@
 #include "PresetSettings.hpp"
 #include "FormReader.hpp"
 #include "Serialization.hpp"
+#include "Papyrus.hpp"
